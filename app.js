bfdbf677fd97b7adbbf9206042a64cde15988bf0
Baggio Shehadi <k3nh1n@yahoo.com>
2018-09-29 11:06:49 -0500

    created 2nd circle

--- app.js
+++ app.js
@@ -7,18 +7,47 @@ canvas.width = window.innerWidth;
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 const startingRadius = 400;
+
+//circle 1
 let circleX = Math.floor(Math.random()*canvasWidth);
 let circleY = Math.floor(Math.random()*canvasHeight);
+//right
 if(circleX+startingRadius>canvasWidth){
     circleX = circleX - startingRadius;
+//left
 }else if(circleX-startingRadius<0){
     circleX+= startingRadius;
 }
+//bottom
 if(circleY+startingRadius>canvasHeight){
     circleY-= startingRadius;
+//top
 }else if(circleY-startingRadius<0){
     circleY+= startingRadius;
 }
 c.beginPath();
 c.arc(circleX,circleY,startingRadius,0,Math.PI *2);
 c.stroke();
+
+//circle 2
+let circle2X = Math.floor(Math.random()*(startingRadius*2))+circleX-startingRadius
+let circle2Y = Math.floor(Math.random()*(startingRadius*2))+circleY-startingRadius
+let radius2 = startingRadius/2;
+if(circle2X+radius2>circleX+startingRadius){
+    let diff = circle2X -circleX;
+    circle2X = circle2X - diff;
+}else if(circle2X-radius2<circleX-startingRadius){
+    let diff = circle2X -circleX;
+    circle2X = circle2X - diff;
+}
+if(circle2Y+radius2>circleY+startingRadius){
+    let diff = circle2Y-circleY;
+    circle2Y = circle2Y - diff
+}else if(circle2Y-radius2<circleY-startingRadius){
+    let diff = circle2Y-circleY;
+    circle2Y = circle2Y - diff 
+}
+c.beginPath();
+c.arc(circle2X,circle2Y,radius2,0,Math.PI*2);
+c.stroke()
+//TODO: Fix bug where circle can go outside because corners
